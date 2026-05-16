013d28f28e0a3375b07e9b83bba5a58bb3e2ac2c
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-16 14:07:42 -0300

    menu modularizado

--- home.php
+++ home.php
@@ -11,15 +11,13 @@
 </head>
 <body>
     <header>
-        <img src="midias/logo-temakeria-bia.jpg" alt="Logo Temakeria da Bia">
-        <h1>Temakeria da Bia</h1>
-        <nav><i class="fa-solid fa-angle-down" id="btn-menu"></i></nav>
+        <?php include("menu.php"); ?>
     </header>
     <main>
 
     </main>
     <footer>
-
+        
     </footer>
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,3 @@
+<img src="midias/logo-temakeria-bia.jpg" alt="Logo Temakeria da Bia">
+<h1>Temakeria da Bia</h1>
+<nav><i class="fa-solid fa-angle-down" id="btn-menu"></i></nav>
