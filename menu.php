3b7df71a596ff85af2d204e05f1f1285cfc53975
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-16 15:03:00 -0300

    Menu hamburguer em JS

--- menu.php
+++ menu.php
@@ -1,3 +1,29 @@
 <img src="midias/logo-temakeria-bia.jpg" alt="Logo Temakeria da Bia">
 <h1>Temakeria da Bia</h1>
-<nav><i class="fa-solid fa-angle-down" id="btn-menu"></i></nav>
+<nav id="menu">
+    <i class="fa-solid fa-angle-down" onclick="toggleMenu()" id="icon"></i>
+    <ul id="lista-menu">
+        <li><a href="#">Nossos produtos</a></li>
+        <li><a href="#">Seus pedidos</a></li>
+        <li><a href="#">Sua conta</a></li>
+        <li><a href="#">Configurações</a></li>
+    </ul>
+</nav>
+
+<script>
+    //Script para menu hamburguer
+    function toggleMenu(){
+        let lista = document.getElementById('lista-menu')
+        let icone = document.getElementById('icon')
+
+        lista.classList.toggle('aberto')
+
+        if(lista.classList.contains('aberto')){
+            icone.classList.remove('fa-angle-down')
+            icone.classList.add('fa-angle-up')
+        }else{
+            icone.classList.remove('fa-angle-up')
+            icone.classList.add('fa-angle-down')
+        }
+    }
+</script>
