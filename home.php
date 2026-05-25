581fefcf09ffc8ea75204eec8c9f9d5cb45a529c
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-25 15:48:17 -0300

    adicionado cards ao home

--- home.php
+++ home.php
@@ -15,6 +15,28 @@ $usuario = usuario_logado();
 
     <!--Script do fontAwesome para adicionar icones-->
     <script src="https://kit.fontawesome.com/52ff4c741b.js" crossorigin="anonymous"></script>
+
+    <style>
+        section#intro{
+            background: black url("midias/foto-temaki-intro.jpg") center center fixed;
+            background-size: cover;
+            padding: 20px;
+            width: 100vw !important;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            height: 100%;
+        }
+
+        div.conteudo{
+            background-color: #ffffffc9;
+            border-radius: 5px;
+            padding: 10px;
+            text-align: center;
+            margin: 20px 0px;
+            width: 450px;
+        }
+    </style>
 </head>
 <body>
     <div id="separador">
@@ -22,6 +44,19 @@ $usuario = usuario_logado();
             <?php include("menu.php"); ?>
         </header>
         <main>
+            <section id="intro">
+                <div class="conteudo">
+                    <h2>Bem vindo ao nosso webSite</h2>
+                    <p>Aqui você pode realizar pedidos, conhecer um pouco mais sobre a gente e acompanhar de perto nossas principais novidades!</p>
+                </div>
+                <div class="conteudo">
+                    <h2>Novo por aqui!?</h2>
+                    <p>Faça seu login para ter gerenciar seus pedidos e preferências</p>
+                </div>
+                <div class="conteudo">
+                    <h2>Notícias da Bia</h2>
+                </div>
+            </section>
            <?php include("cardapio.php"); ?>
         </main>
     </div>
