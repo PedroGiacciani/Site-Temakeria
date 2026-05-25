5cb452a1258e9e0399d1119a8d6bbd280ca20653
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-25 14:49:36 -0300

    Atualizações

--- home.php
+++ home.php
@@ -15,6 +15,26 @@ $usuario = usuario_logado();
 
     <!--Script do fontAwesome para adicionar icones-->
     <script src="https://kit.fontawesome.com/52ff4c741b.js" crossorigin="anonymous"></script>
+
+    <style>
+        main{
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: center;
+        }
+
+        main > h1{
+            text-align: center;
+            margin: 10px;
+        }
+
+        section{
+            width: 450px;
+            height: 300px;
+            border: 1px solid black;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 <body>
     <div id="separador">
@@ -22,6 +42,16 @@ $usuario = usuario_logado();
             <?php include("menu.php"); ?>
         </header>
         <main>
+            <h1>Olá, seja bem vindo ao nosso webSite</h1>
+            <section>
+                <a href="#cardapio-ancora" onclick="mostrarCardapio()">Confira nossos deliciosos produtos!</a>
+            </section>
+            <section>
+                <a href="login.php">Faça login para conferir seus pedidos!</a>
+            </section>
+            <section>
+                <a href="quem-somos.php">Confira nossa história!</a>
+            </section>
            <?php include("cardapio.php"); ?>
         </main>
     </div>
